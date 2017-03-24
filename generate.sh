21a68f970729bfb50c05a7ff958cfd3afbeffb2b
Frank Schumacher <frank.schumacher@telefonica.com>
2017-03-24 14:04:39 +0100

    prepare for new run

--- generate.sh
+++ generate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Generates merged sdoc
 
-RAILS_VERSION="v4.2.6"
-RUBY_VERSION="2.3.0"
+RAILS_VERSION="v5.0.2"
+RUBY_VERSION="2.3.1"
 
 gem install sdoc
 
@@ -14,12 +14,13 @@ cd repos
 # Rails
 echo "Fetching Rails $RAILS_VERSION repo from github.com"
 rm -rf rails
-git clone https://github.com/rails/rails.git
+git clone --branch  --single-branch ${RAILS_VERSION} https://github.com/rails/rails.git
 cd rails
 git ch $RAILS_VERSION
 
-echo "Generating SDOC"
-sdoc -o ../../sdocs/rails-$RAILS_VERSION --line-numbers --format=sdoc -T rails --github .
+echo "Generating SDOC for Rails #{RAILS_VERSION}"
+sdoc -o ../../sdocs/rails-$RAILS_VERSION --line-numbers --format=sdoc -T rails --github . &
+RAILS_PID=$!
 
 # Ruby
 echo "Fetching ruby $RUBY_VERSION from ruby-lang.org"
@@ -29,8 +30,12 @@ cd ruby
 curl -o ruby.tar.bz2 http://ftp.ruby-lang.org/pub/ruby/ruby-$RUBY_VERSION.tar.bz2
 tar xjf ruby.tar.bz2
 cd ruby-$RUBY_VERSION
-echo "Generating SDOC"
-sdoc -o ../../../sdocs/ruby-$RUBY_VERSION --line-numbers --format=sdoc -T rails --github .
+echo "Generating SDOC for ruby ${RUBY_VERSION}"
+sdoc -o ../../../sdocs/ruby-$RUBY_VERSION --line-numbers --format=sdoc -T rails --github . &
+RUBY_PID=$!
+
+echo "Waiting for RUBY and RAILS sdoc commands to finish"
+wait $RAILS_PID $RUBY_PID
 
 # Merge sdocs
 cd ../../../sdocs
@@ -44,5 +49,5 @@ rm -rf rails-$RAILS_VERSION ruby-$RUBY_VERSION
 rm -rf ../repos
 
 echo "================================"
-echo "Merged SDOC is now in rails-ruby"
+echo "Merged SDOC is now in sdocs/rails-ruby"
 echo "================================"
